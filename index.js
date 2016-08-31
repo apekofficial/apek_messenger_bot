dbd84d55b81b14a7020805fa53791c77d78ec131
Joshua Gilbert <Joshua@Joshuas-MacBook-Pro.local>
2016-08-31 13:30:42 -0700

    tryng to fix bug

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.post('/webhook', function (req, res) {
         var event = events[i];
         if (event.message && event.message.text) {
 
-            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+            ApekMessage(event.sender.id, {text: event.message.text});
         }
     }
     res.sendStatus(200);
